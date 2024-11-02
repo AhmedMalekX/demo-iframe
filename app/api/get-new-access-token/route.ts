5be30b288590ff14eb38358e2c899b2b69e40e18
Ahmed Malek <ahmedmalik1w@gmail.com>
2024-11-02 12:58:51 +0200

    test vercel #3

--- app/api/get-new-access-token/route.ts
+++ app/api/get-new-access-token/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET || "your_access_secret_key";
+
 const REFRESH_TOKEN_SECRET =
   process.env.REFRESH_TOKEN_SECRET || "your_refresh_secret_key";
 const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN || "15m";
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,27 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.PARENT_SITE_URL!,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
