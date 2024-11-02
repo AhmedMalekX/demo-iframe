8641d5f6a382a6514356be8ff318ed705401d950
Ahmed Malek <ahmedmalik1w@gmail.com>
2024-11-02 13:41:32 +0200

    update the headers in the next.config file

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,15 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/",
+        source: "/:path*",
         headers: [
           {
             key: "Access-Control-Allow-Origin",
             value: "https://demo-iframe-parent.vercel.app",
           },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://demo-iframe-parent.vercel.app",
           },
         ],
       },
