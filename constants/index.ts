e85c5cccea165b1603e9689ab2e495b551ba2232
Ahmed Malek <ahmedmalik1w@gmail.com>
2024-11-11 01:08:48 +0200

    feat(Dashboard State Management): Add store for managing dashboard state.
    This commit introduces a store to handle the state management for the dashboard component using Zustand. The new store centralizes the state related to the dashboard, making it easier to manage active data, interactions, and UI-related behaviors from a single source. This improves the scalability and maintainability of the code by simplifying state updates and enhancing component communication.
    
    BREAKING CHANGE: None
    
    Closes Closes #21.

--- constants/index.ts
+++ constants/index.ts
@@ -10,3 +10,78 @@ export const GeneratingImagesMethods: IGeneratingImagesMethods[] = [
   "From image",
   "From elements",
 ];
+
+export const Styles = [
+  {
+    styleName: "Kawaii",
+    styleImage: "/styles/kawaii.jpeg",
+  },
+  {
+    styleName: "Watercolour",
+    styleImage: "/styles/watercolour.jpeg",
+  },
+  {
+    styleName: "Lowpoly",
+    styleImage: "/styles/lowpoly.jpeg",
+  },
+  {
+    styleName: "Isometric",
+    styleImage: "/styles/isometric.jpeg",
+  },
+  // {
+  //   styleName: "Anime",
+  //   styleImage: "/styles/anime.jpeg",
+  // },
+  {
+    styleName: "Papercut",
+    styleImage: "/styles/papercut_layered.jpeg",
+  },
+  {
+    styleName: "Ink print",
+    styleImage: "/styles/ink_print.jpeg",
+  },
+  {
+    styleName: "Line art",
+    styleImage: "/styles/line_art.jpeg",
+  },
+  {
+    styleName: "Tribal",
+    styleImage: "/styles/tribal.jpeg",
+  },
+  {
+    styleName: "Cubism",
+    styleImage: "/styles/cubism.jpeg",
+  },
+  {
+    styleName: "Comic book",
+    styleImage: "/styles/comic_book.jpeg",
+  },
+  {
+    styleName: "Minimalist",
+    styleImage: "/styles/minimalism.jpeg",
+  },
+  {
+    styleName: "Paper quilling",
+    styleImage: "/styles/paper_quilling.jpeg",
+  },
+  {
+    styleName: "Stained glass",
+    styleImage: "/styles/stained_glass.jpeg",
+  },
+  {
+    styleName: "Pop art",
+    styleImage: "/styles/pop_art.jpeg",
+  },
+  {
+    styleName: "Zentangle",
+    styleImage: "/styles/zentangle.jpeg",
+  },
+  {
+    styleName: "Oil painting",
+    styleImage: "/styles/oil_painting.jpeg",
+  },
+  {
+    styleName: "Origami",
+    styleImage: "/styles/origami.jpeg",
+  },
+];
---
+++ store/dashboard.store.ts
@@ -0,0 +1,136 @@
+/*
+ * Packages
+ * */
+import { create } from "zustand";
+
+/*
+ * Constants
+ * */
+import { Styles } from "@/constants";
+
+interface DashboardStore {
+  // General options
+  guidanceScale: number;
+  setGuidanceScale: (value: number) => void;
+  sizeOfImageOnFromTextTab: number;
+  setSizeOfImageOnFromTextTab: (value: number) => void;
+  sizeOfImageOnFromImageTab: number;
+  setSizeOfImageOnFromImageTab: (value: number) => void;
+  numberOfImages: number;
+  setNumberOfImages: (value: number) => void;
+  openStylesModal: boolean;
+  setOpenStylesModal: (value: boolean) => void;
+  styleName: (typeof Styles)[number]["styleName"] | undefined;
+  setStyleName: (
+    value: (typeof Styles)[number]["styleName"] | undefined,
+  ) => void;
+
+  // Generate image using another image
+  selectedGenerateSimilarImage: string | null;
+  setSelectedGenerateSimilarImage: (imageUrl: string | null) => void;
+  generationMethod: "Variation" | "Image Mixing";
+  setGenerationMethod: (value: "Variation" | "Image Mixing") => void;
+
+  // Generate image using elements
+  generatingMotif: boolean;
+  setGeneratingMotif: (value: boolean) => void;
+
+  // Result
+  allDataLength: number;
+  setAllDataLength: (value: number) => void;
+  allData: [];
+  setAllData: (value: []) => void;
+  dataLoading: boolean;
+  setDataLoading: (value: boolean) => void;
+  showLoadingCards: boolean;
+  setShowLoadingCards: (value: boolean) => void;
+  showGeneratedImages: boolean;
+  setShowGeneratedImages: (value: boolean) => void;
+  keepOutline: number;
+  setKeepOutline: (value: number) => void;
+  keepStyle: number;
+  setKeepStyle: (value: number) => void;
+
+  // Submitting the form to get the data
+  submittingFromToGetData: boolean;
+  setSubmittingFromToGetData: (value: boolean) => void;
+
+  // Handle error
+  showErrorModal: boolean;
+  setShowErrorModal: (value: boolean) => void;
+  errorModalMessage: {
+    header: string | null;
+    body: string | null;
+  };
+  setErrorModalMessage: (value: {
+    header: string | null;
+    body: string | null;
+  }) => void;
+
+  // Feedback
+  showCreatorReview: boolean;
+  setShowCreatorReview: (value: boolean) => void;
+}
+
+export const useImagesStore = create<DashboardStore>((set) => ({
+  // General options
+  guidanceScale: 7,
+  setGuidanceScale: (value) => set({ guidanceScale: value }),
+  sizeOfImageOnFromTextTab: 512,
+  setSizeOfImageOnFromTextTab: (value) =>
+    set({ sizeOfImageOnFromTextTab: value }),
+  sizeOfImageOnFromImageTab: 512,
+  setSizeOfImageOnFromImageTab: (value) =>
+    set({ sizeOfImageOnFromImageTab: value }),
+  numberOfImages: 2,
+  setNumberOfImages: (value) => set({ numberOfImages: value }),
+  openStylesModal: false,
+  setOpenStylesModal: (value) => set({ openStylesModal: value }),
+  styleName: undefined,
+  setStyleName: (value) => set({ styleName: value }),
+
+  // Generate image using another image
+  selectedGenerateSimilarImage: null,
+  setSelectedGenerateSimilarImage: (state) =>
+    set({ selectedGenerateSimilarImage: state }),
+  generationMethod: "Variation",
+  setGenerationMethod: (value) => set({ generationMethod: value }),
+
+  // Generate image using elements
+  generatingMotif: false,
+  setGeneratingMotif: (value) => set({ generatingMotif: value }),
+
+  // Result
+  allDataLength: 0,
+  setAllDataLength: (value) => set({ allDataLength: value }),
+  allData: [],
+  setAllData: (value) => set({ allData: value }),
+  dataLoading: false,
+  setDataLoading: (value) => set({ dataLoading: value }),
+  showLoadingCards: false,
+  setShowLoadingCards: (value) => set({ showLoadingCards: value }),
+  showGeneratedImages: false,
+  setShowGeneratedImages: (value) => set({ showGeneratedImages: value }),
+  keepOutline: 0.7,
+  setKeepOutline: (value) => set({ keepOutline: value }),
+  keepStyle: 0.7,
+  setKeepStyle: (value) => set({ keepStyle: value }),
+
+  // Submitting the form to get the data
+  submittingFromToGetData: false,
+  setSubmittingFromToGetData: (value) =>
+    set({ submittingFromToGetData: value }),
+
+  // Handle error
+  showErrorModal: false,
+  setShowErrorModal: (value) => set({ showErrorModal: value }),
+  errorModalMessage: {
+    header: null,
+    body: null,
+  },
+  setErrorModalMessage: (value) =>
+    set({ errorModalMessage: { header: value.header, body: value.body } }),
+
+  showCreatorReview: false,
+  setShowCreatorReview: (value) => set({ showCreatorReview: value }),
+}));
