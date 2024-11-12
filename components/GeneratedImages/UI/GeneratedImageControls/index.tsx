62257bf3505352f02b7b444933d300922f836a4a
Ahmed Malek <ahmedmalik1w@gmail.com>
2024-11-12 18:26:02 +0200

    feat(UI Components): Add Popover component and Download button
    - Created a reusable Popover component to enhance UI interactivity.
    - Added a Download button within the GeneratedImageControls to allow users to download the generated images directly from the interface.
    - These components improve the user experience by offering image preview zoom and download capabilities, neatly encapsulated for future scalability.
    
    BREAKING CHANGE: None
    
    Closes #31

--- components/GeneratedImages/UI/GeneratedImageControls/index.tsx
+++ components/GeneratedImages/UI/GeneratedImageControls/index.tsx
@@ -12,9 +12,25 @@ import { useDashboardStore } from "@/store/dashboard.store";
  * UI Components
  * */
 import { Slider } from "@/components/ui/slider";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+
+/*
+ * Icons
+ * */
+import { Download } from "lucide-react";
 
 export const GeneratedImageControls = () => {
   const { imagePreviewZoom, setImagePreviewZoom } = useDashboardStore();
+
+  /*
+   * TODO:
+   *  1- Add download options
+   * */
 
   return (
     <div className="py-4 flex items-center justify-between w-full">
@@ -34,7 +50,17 @@ export const GeneratedImageControls = () => {
         />
         <span className="font-medium">{imagePreviewZoom}%</span>
       </div>
-      <div>Download</div>
+      <div>
+        <Popover>
+          <PopoverTrigger>
+            <Button variant="primary" className="rounded-2xl">
+              <Download />
+              <span>Download</span>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent align="end">Download options!</PopoverContent>
+        </Popover>
+      </div>
     </div>
   );
 };
