8a12b7ae8bfa30ac4b5a7aa6dd341f56229c45c7
Ahmed Malek <ahmedmalik1w@gmail.com>
2024-11-18 00:32:58 +0200

    debug vercel #3

--- next.config.ts
+++ next.config.ts
@@ -45,12 +45,11 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value:
-              "https://dev-demo-iframe-parent.vercel.app/ https://demo-iframe-parent-roan.vercel.app/ http://localhost:3000/",
+            value: "https://dev-demo-iframe-parent.vercel.app",
           },
           {
             key: "Content-Security-Policy",
-            value: `frame-ancestors 'self' https://dev-demo-iframe-parent.vercel.app/ https://demo-iframe-parent-roan.vercel.app/ http://localhost:3000/`,
+            value: `frame-ancestors 'self' https://dev-demo-iframe-parent.vercel.app`,
           },
         ],
       },
